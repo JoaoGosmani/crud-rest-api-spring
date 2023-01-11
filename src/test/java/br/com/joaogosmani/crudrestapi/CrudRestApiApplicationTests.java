813613aa8b3ab6ddb956ef86103978aeeffd2744
Joao Gosmani <joaogosmani@gmail.com>
2023-01-11 15:37:49 -0300

    Projeto criado

---
+++ src/test/java/br/com/joaogosmani/crudrestapi/CrudRestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.joaogosmani.crudrestapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudRestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
