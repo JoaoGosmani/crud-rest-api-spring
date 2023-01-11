17d2daa8135ba01102b72634ab3094cef5bda70c
Joao Gosmani <joaogosmani@gmail.com>
2023-01-11 16:45:11 -0300

    Criando o modelo Usuario e sua tabela no banco

---
+++ src/main/java/br/com/joaogosmani/crudrestapi/models/Usuario.java
@@ -0,0 +1,73 @@
+package br.com.joaogosmani.crudrestapi.models;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Usuario implements Serializable{
+
+	private static final long serialVersionUID = 1L;
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	private String nome;
+	
+	private Integer idade;
+	
+	public Usuario() {}
+
+	public Usuario(Long id, String nome, Integer idade) {
+		this.id = id;
+		this.nome = nome;
+		this.idade = idade;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public Integer getIdade() {
+		return idade;
+	}
+
+	public void setIdade(Integer idade) {
+		this.idade = idade;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Usuario other = (Usuario) obj;
+		return Objects.equals(id, other.id);
+	}
+
+}
